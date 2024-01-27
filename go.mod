7aca85cfa9e5fcf7a41008b2c45f26d4c7310afa
gambi <cjmduffey88@gmail.com>
2024-01-27 03:09:49 +0000

    Initial commit

---
+++ go.mod
@@ -0,0 +1,1 @@
+module github.com/cjmduffey88/utils
---
+++ utils.go
@@ -0,0 +1,29 @@
+package utils
+
+import "os"
+
+func HandleError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func LoadFile(path string) []byte {
+	file, err := os.Open(path)
+	HandleError(err)
+	defer file.Close()
+	stat, err := file.Stat()
+	HandleError(err)
+	data := make([]byte, stat.Size())
+	_, err = file.Read(data)
+	HandleError(err)
+	return data
+}
+
+func AddLog(path, log string) {
+	file, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	HandleError(err)
+	defer file.Close()
+	_, err = file.WriteString(log + "\n")
+	HandleError(err)
+}
